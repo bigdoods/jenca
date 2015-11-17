665990e26138db7ecba4366066e47617a361bc74
Daniel Craig <daniel@danielcraig.me>
2015-11-17 20:46:35 +0000

    new commands to install jenca cloud platform

--- install.sh
+++ install.sh
@@ -1,26 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
-#install docker
-wget -qO- https://get.docker.com/ | sh
-VERSION=`docker --version | egrep -o "([0-9]{1,}\.)+[0-9]{1,}"`
+apt-get install -y make curl
 
-if [[ -z "$VERSION" ]]; then
-        echo "fail"
-        exit 1
-fi
+# install docker
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker vagrant
 
-if [[ -z "$TOMCAT_USER" ]]; then
-        export TOMCAT_USER=xxx
-fi
-
-if [[ -z "$TOMCAT_PASSWORD" ]]; then
-        export TOMCAT_PASSWORD=xxx
-fi
-
-#intall Dockerfile
-docker run -d \
-        -e TOMCAT_USER=$TOMCAT_USER \
-        -e TOMCAT_PASSWORD=$TOMCAT_PASSWORD \
-        -p 8080:8080 \
-        --restart=always \
-        bimscript/docker-bimserver
+# install docker compose
+curl -L \
+    https://github.com/docker/compose/releases/download/1.5.0rc1/docker-compose-`uname -s`-`uname -m` \
+    > /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
